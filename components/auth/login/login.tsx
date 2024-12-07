6bebaba9a93085db170d3108d23ad032378ab323
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-07 11:38:31 +0530

    feat: add login ui component

---
+++ components/auth/login/login.tsx
@@ -0,0 +1,56 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "./FormContent";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export const LoginComponent = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <Card className="w-[400px] ">
+          <CardHeader>
+            <div className="flex flex-col space-y-2 text-center">
+              <h1 className="text-2xl font-semibold tracking-tight">
+                Create an account
+              </h1>
+              <p className="text-sm text-muted-foreground">
+                Enter your email below to create your account
+              </p>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <UserAuthForm />
+          </CardContent>
+          <CardFooter>
+            <p className="px-8 text-center text-sm text-muted-foreground">
+              By clicking continue, you agree to our{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Terms of Service
+              </Link>{" "}
+              and{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
+};
