1a79d30ee1f8e5d64cb96c21544c9dd30be3754e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-07 14:22:02 +0530

    feat: optimize login component

--- components/auth/login/login.tsx
+++ components/auth/login/login.tsx
@@ -31,23 +31,17 @@ export const LoginComponent = () => {
             <UserAuthForm />
           </CardContent>
           <CardFooter>
-            {/* <p className="px-8 text-center text-sm text-muted-foreground">
-              By clicking continue, you agree to our{" "}
+            <p className="px-10 text-center text-sm text-muted-foreground">
+              Already have an account?
               <Link
-                href="/terms"
+                href="/auth/register"
                 className="underline underline-offset-4 hover:text-primary"
               >
-                Terms of Service
-              </Link>{" "}
-              and{" "}
-              <Link
-                href="/privacy"
-                className="underline underline-offset-4 hover:text-primary"
-              >
-                Privacy Policy
+                {" "}
+                Register
               </Link>
               .
-            </p> */}
+            </p>
           </CardFooter>
         </Card>
       </div>
