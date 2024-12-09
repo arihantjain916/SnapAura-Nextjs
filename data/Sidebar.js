9dad6ef5c9126c44a2bfac5394dc0821f60c47aa
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-09 14:13:48 +0530

    feat: move mobile navbar in seprate component

---
+++ data/Sidebar.js
@@ -0,0 +1,38 @@
+import {
+  Frame,
+  Map,
+  Bell,
+  Home,
+  Search,
+  MessageCircle,
+  User,
+  Plus,
+} from "lucide-react";
+
+export const DATA = [
+  {
+    name: "Home",
+    url: "/",
+    icon: Home,
+  },
+  {
+    name: "Notification",
+    url: "/notification",
+    icon: Bell,
+  },
+  {
+    name: "Create",
+    url: "/create",
+    icon: Plus,
+  },
+  {
+    name: "Chat",
+    url: "/chat",
+    icon: MessageCircle,
+  },
+  {
+    name: "Profile",
+    url: "/profile",
+    icon: User,
+  },
+];
