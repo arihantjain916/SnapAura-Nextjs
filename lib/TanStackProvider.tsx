958f7580e52d45642bdde5a3829a9827971f9002
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-09 15:36:48 +0530

    fix: update tanstack provider

--- lib/TanStackProvider.tsx
+++ lib/TanStackProvider.tsx
@@ -1,15 +1,14 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+"use client";
 
-const queryClient = new QueryClient();
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
 
 export const TanStackProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
+  const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
