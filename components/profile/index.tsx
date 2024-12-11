fedf63e00042c5b7546afa5172c627b7203cdec3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-11 14:15:47 +0530

    feat: add logout button on profile page

--- components/profile/index.tsx
+++ components/profile/index.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
   Card,
   CardContent,
@@ -5,10 +7,21 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useDispatch } from "react-redux";
+import { logout } from "@/redux/features/auth";
+import { useRouter } from "next/navigation";
 
 import { FormContent } from "./FormContent";
 
 export function Profile() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    router.push("/auth/login");
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
       <Card className="w-[350px] mr-4 md:w-[500px] md:mr-0">
@@ -22,6 +35,13 @@ export function Profile() {
           <FormContent />
         </CardContent>
       </Card>
+
+      <button
+        className="absolute top-4 right-4 md:top-8 md:right-8 rounded-md bg-red-500 px-4 py-2 text-white"
+        onClick={logoutHandler}
+      >
+        Logout
+      </button>
     </div>
   );
 }
