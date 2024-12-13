805f4ee6b5674d7df901f1c70a8d6b08904d2a61
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-13 13:55:12 +0530

    feat: make like component working

--- components/feed/feed.tsx
+++ components/feed/feed.tsx
@@ -1,22 +1,32 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Heart } from "lucide-react";
 import AxiosInstance from "@/lib/axiosInstance";
+import Cookies from "js-cookie";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Feed = () => {
   async function fetchFeed() {
     try {
-      const res = await AxiosInstance.get("/post");
+      const res = await AxiosInstance.get("/post", {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("AUTH_TOKEN")}`,
+        },
+      });
       return res.data.data;
     } catch (err) {
       throw err;
     }
   }
+
+  const queryClient = useQueryClient();
+
   const { isPending, error, data } = useQuery({
     queryKey: ["feedData"],
     queryFn: async () => await fetchFeed(),
-    refetchInterval: 50000,
+    refetchInterval: 500000,
   });
 
   if (error) {
@@ -25,7 +35,7 @@ export const Feed = () => {
         <div className="p-8 flex justify-center">
           <div className="rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3">
             <div className="px-3 pb-2">
-              <h1 className="text-center text-2xl font-bold">
+              <h1 className="text-center text-2xl font-bold dark:text-white">
                 Something went wrong
               </h1>
             </div>
@@ -49,71 +59,104 @@ export const Feed = () => {
     );
   }
 
+  async function handleLike(id: number) {
+    try {
+      const res = await AxiosInstance.post(
+        `/post/like/${id}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("AUTH_TOKEN")}`,
+          },
+        }
+      );
+
+      if (res.data.success) {
+        queryClient.invalidateQueries({ queryKey: ["feedData"] });
+      }
+    } catch (error: any) {
+      if (error.response.status === 422) {
+        toast.warn(error.response.data.message, {
+          position: "bottom-right",
+        });
+      } else {
+        toast.warn(error.response.data.message, {
+          position: "bottom-right",
+        });
+      }
+    }
+  }
+
   return (
-    <div className="container mx-auto my-10 sm:px-20">
-      {data?.map((feed: any) => (
-        <div key={feed.id} className="p-8 flex justify-center">
-          <div className="rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3 dark:bg-gray-800">
-            {/* Header Section */}
-            <div className="w-full flex justify-between p-3">
-              <div className="flex items-center">
-                <div className="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
-                  <img src={feed.user.profile} alt={feed.user.username} />
-                </div>
-                <span className="pt-1 ml-2 font-bold text-sm dark:text-white">
-                  {feed.user.username}
-                </span>
-              </div>
-              <span className="px-2 hover:bg-gray-300 cursor-pointer rounded">
-                <i className="fas fa-ellipsis-h pt-2 text-lg"></i>
-              </span>
-            </div>
-
-            {/* Image Section */}
-            <img
-              className="w-full bg-cover pointer-events-none"
-              src={feed.image}
-              alt={feed.user.username}
-            />
-
-            {/* Content Section */}
-            <div className="px-3 pb-2">
-              {/* Likes */}
-              <div className="pt-2 flex gap-2 items-center">
-                <button>
-                  <Heart />
-                </button>
-                <span className="text-base mb-2 cursor-pointer">
-                  {feed.totalLikes} likes
-                </span>
-              </div>
-
-              {/* Description */}
-              <div className="pt-1">
-                <div className="mb-2 text-sm">
-                  <span className="font-medium mr-2">{feed.user.username}</span>
-                  {feed.caption}
-                </div>
-              </div>
-
-              {/* Comments */}
-              {feed.comments.length > 0 && (
-                <>
-                  <div className="text-base mb-2 cursor-pointer">
-                    View all {feed.comments.length} comments
+    <>
+      <ToastContainer />
+      <div className="container mx-auto my-10 sm:px-20">
+        {data?.map((feed: any) => (
+          <div key={feed.id} className="p-8 flex justify-center">
+            <div className="rounded overflow-hidden border w-full lg:w-6/12 md:w-6/12 bg-white mx-3 dark:bg-gray-800">
+              {/* Header Section */}
+              <div className="w-full flex justify-between p-3">
+                <div className="flex items-center">
+                  <div className="rounded-full h-8 w-8 bg-gray-500 flex items-center justify-center overflow-hidden">
+                    <img src={feed.user.profile} alt={feed.user.username} />
                   </div>
+                  <span className="pt-1 ml-2 font-bold text-sm dark:text-white">
+                    {feed.user.username}
+                  </span>
+                </div>
+                <span className="px-2 hover:bg-gray-300 cursor-pointer rounded">
+                  <i className="fas fa-ellipsis-h pt-2 text-lg"></i>
+                </span>
+              </div>
+
+              {/* Image Section */}
+              <img
+                className="w-full bg-cover pointer-events-none"
+                src={feed.image}
+                alt={feed.user.username}
+              />
+
+              {/* Content Section */}
+              <div className="px-3 pb-2">
+                {/* Likes */}
+                <div className="pt-2 flex gap-2 items-center">
+                  <button onClick={() => handleLike(feed.id)}>
+                    <Heart className={feed.isLiked && "fill-red-500"} />
+                  </button>
+                  <span className="text-base mb-2 cursor-pointer">
+                    {feed.totalLikes} likes
+                  </span>
+                </div>
+
+                {/* Description */}
+                <div className="pt-1">
                   <div className="mb-2 text-sm">
                     <span className="font-medium mr-2">
-                      {feed.comments[0].user.username}
+                      {feed.user.username}
                     </span>
-                    {feed.comments[0].comment}
+                    {feed.caption}
                   </div>
-                </>
-              )}
+                </div>
+
+                {/* Comments */}
+                {feed.comments.length > 0 && (
+                  <>
+                    <div className="text-base mb-2 cursor-pointer">
+                      View all {feed.comments.length} comments
+                    </div>
+                    <div className="mb-2 text-sm">
+                      <span className="font-medium mr-2">
+                        {feed.comments[0].user.username}
+                      </span>
+                      {feed.comments[0].comment}
+                    </div>
+                  </>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
