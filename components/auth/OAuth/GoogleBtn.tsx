4e5747ecec0b03839aecd050f85849f54b61b7a3
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-24 13:58:01 +0530

    feat: make googleLoginBtn working

--- components/auth/OAuth/GoogleBtn.tsx
+++ components/auth/OAuth/GoogleBtn.tsx
@@ -1,8 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/ui/icons";
+import AxiosInstance from "@/lib/axiosInstance";
+import axios from "axios";
 
 export const GoogleBtn = () => {
-  async function handleGoogleSignIn() {}
+  async function handleGoogleSignIn() {
+    try{
+        const res = await axios.get("http://localhost:8000/api/oauth/google");
+        console.log(res.data);
+        if(res.data.status === 'success'){
+            window.location.href = res.data.url;
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+  }
   return (
     <Button variant="outline" type="button" onClick={handleGoogleSignIn}>
       <Icons.google className="mr-2 h-4 w-4" />
