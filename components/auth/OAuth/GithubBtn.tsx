a0960e014875d4a0fe53a5fccdfbc6a735d02732
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-24 13:58:41 +0530

    feat: make gitHubLoginBtn working

--- components/auth/OAuth/GithubBtn.tsx
+++ components/auth/OAuth/GithubBtn.tsx
@@ -1,8 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/ui/icons";
+import axios from "axios";
 
 export const GitHubBtn = () => {
-  async function handleGitHubLogin() {}
+  async function handleGitHubLogin() {
+    try {
+      const res = await axios.get("http://localhost:8000/api/oauth/github");
+      console.log(res.data);
+      if (res.data.status === "success") {
+        window.location.href = res.data.url;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <Button variant="outline" type="button" onClick={handleGitHubLogin}>
       <Icons.gitHub className="mr-2 h-4 w-4" />
