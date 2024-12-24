3e321adf3f1cc671a9f3eeb62e7f9675d545cd88
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-24 15:08:07 +0530

    feat: update callback function

--- components/auth/OAuth/Callback.tsx
+++ components/auth/OAuth/Callback.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import axios from "axios";
-import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useParams, useSearchParams } from "next/navigation";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
@@ -9,7 +8,6 @@ import { userdata } from "@/redux/features/auth";
 import AxiosInstance from "@/lib/axiosInstance";
 
 export const OAuthCallback = () => {
-  const pathname = usePathname();
   const searchParams = useSearchParams();
   const params = useParams<{ provider: string }>();
 
@@ -18,16 +16,9 @@ export const OAuthCallback = () => {
 
   async function handleCallback() {
     try {
-      //   const res = await axios.get(
-      //     `http://localhost:8000/api/oauth/${params.provider}/callback?code=${searchParams.get(
-      //       "code"
-      //     )}`
-      //   );
-
       const res = await AxiosInstance.get(
         `/oauth/${params.provider}/callback?code=${searchParams.get("code")}`
       );
-      console.log(res.data);
       if (res.data.status === "success") {
         Cookies.set("AUTH_TOKEN", res.data.token, { expires: 7 });
         dispatch(
@@ -42,7 +33,7 @@ export const OAuthCallback = () => {
         router.push("/");
       }
     } catch (err) {
-        console.log(err);
+      console.log(err);
       router.push("/auth/login");
     }
   }
