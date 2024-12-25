359d48c6c3d0e45de822e4542bd9d89b2936ec71
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-25 10:01:04 +0530

    feat: make chatcomponent data dynamic

--- components/messages/chatComponent.tsx
+++ components/messages/chatComponent.tsx
@@ -1,17 +1,18 @@
-export const ChatComponent = () => {
+export const ChatComponent = ({conversation}: any) => {
+  console.log(conversation);
   return (
     <>
       <div className="flex-1">
         <div className="flex items-center gap-2 px-3 py-2 border-b border-b-slate-700">
           <div className="h-[42px] w-[42px] shrink-0 rounded-full">
             <img
-              src="https://picsum.photos/750/740"
+              src={conversation.otherParty.profile}
               className="h-full w-full rounded-full object-cover"
               alt=""
             />
           </div>
           <div>
-            <h2 className="text-base text-slate-200">James Rodrigo</h2>
+            <h2 className="text-base text-slate-200">{conversation.otherParty.username}</h2>
             <p className="text-xs text-slate-400">Online 3 min ago</p>
           </div>
         </div>
@@ -81,7 +82,7 @@ export const ChatComponent = () => {
               </div>
             </div>
           </div>
-          <input type="text" />
+          <input type="text" className="w-full bg-slate-800 px-3 py-2 text-slate-200 placeholder:text-slate-400" placeholder="Type a message.." />
         </div>
       </div>
     </>
