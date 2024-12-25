79c46a0865041789081ac733dc4f96852f83465e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-25 09:47:05 +0530

    feat: update message index file

--- components/messages/index.tsx
+++ components/messages/index.tsx
@@ -3,7 +3,7 @@ import { FetchConversation } from "./fetchConversation";
 export const Messages = () => {
   return (
     <>
-      <h1>Messages</h1>
+      <h1>Direct Inbox</h1>
       <FetchConversation />
     </>
   );
