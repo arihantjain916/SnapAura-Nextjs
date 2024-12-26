da0ad0db795dc2784bba1aa85c7adcc3d07b6115
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-26 10:03:25 +0530

    feat: optimize sidebar display logic

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,17 +1,5 @@
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
 import { DynamicProfileIndex } from "@/components/profile/profileIndex";
 
 export default function UsernameHome() {
-  return (
-    <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger />
-        <main className="flex-1 relative z-0">
-          <DynamicProfileIndex />
-        </main>
-      </SidebarProvider>
-    </>
-  );
+  return <DynamicProfileIndex />;
 }
