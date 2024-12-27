4e63ce805d87dc645b05ce4534148a4d39493d28
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-27 17:00:16 +0530

    feat: make helper function to handle date

--- lib/dateFormatter.tsx
+++ lib/dateFormatter.tsx
@@ -1,6 +1,15 @@
-import { formatDistanceToNow } from "date-fns";
+import { formatDistanceToNow, format } from "date-fns";
 
 export const DateFormatter = (date: string) => {
   const timeAgo = formatDistanceToNow(new Date(date), { addSuffix: true });
   return timeAgo;
+};
+
+export const ReturnDate = (date: string) => {
+  // const formattedDate = format(new Date(date), "yyyy-MM-dd");
+  const formattedDate = format(new Date(date), 'dd-MMMM-yy')
+  console.log(formattedDate);
+  
+
+  return formattedDate
 };
