c97184ccadc58e2283f08a5a51c216a795f4b06a
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-28 21:53:06 +0530

    feat: add types in notification component

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -1,10 +1,13 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import Pusher from "pusher-js";
 import AxiosInstance from "@/lib/axiosInstance";
-import { useQuery } from "@tanstack/react-query";
+// import { useQuery } from "@tanstack/react-query";
+import { NotificationTypes } from "@/types/NotificationType";
+
 export const Notifications = () => {
+  const [notification, setNotification] = useState<NotificationTypes[]>([]);
   async function fetchNotification() {
     try {
       const res = await AxiosInstance.get("/notification/fetch", {
@@ -13,23 +16,27 @@ export const Notifications = () => {
         },
       });
 
-      return res.data;
+      setNotification(res.data.notifications);
     } catch (err) {
       throw err;
     }
   }
 
-  const { isPending, error, data } = useQuery({
-    queryKey: ["notificationData"],
-    queryFn: async () => await fetchNotification(),
-    refetchInterval: 500000,
-  });
+  //   const { isPending, error, data } = useQuery({
+  //     queryKey: ["notificationData"],
+  //     queryFn: async () => await fetchNotification(),
+  //     refetchInterval: 500000,
+  //   });
 
-  if (isPending) return <h1>Loading...</h1>;
-  if (error) {
-    console.log(error);
-    return <h1>Something went wrong</h1>;
-  }
+  //   if (isPending) return <h1>Loading...</h1>;
+  //   if (error) {
+  //     console.log(error);
+  //     return <h1>Something went wrong</h1>;
+  //   }
+
+  useEffect(() => {
+    fetchNotification();
+  }, []);
 
   useEffect(() => {
     const pusher = new Pusher("fac20a8dfab0cadc489c", {
@@ -41,16 +48,23 @@ export const Notifications = () => {
 
     const channel = pusher.subscribe("notification");
     channel.bind("notification", (data: any) => {
-      console.log(data);
+        setNotification((prev: any) => [...prev, data.data]);
     });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+      pusher.disconnect();
+    };
   }, []);
 
   return (
     <>
       <h1>Notification</h1>
 
-      {data?.notifications?.map((data: any) => (
+      {notification.map((data: any) => (
         <div key={data.id}>
+          <p>{data.id}</p>
           <h1>{data.message}</h1>
         </div>
       ))}
