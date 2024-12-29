21b9dc9be56fc0fd1480a052b81ce82de8f8e296
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-29 19:12:20 +0530

    feat: add pusher config to manage real time notification

--- components/notification/index.tsx
+++ components/notification/index.tsx
@@ -5,8 +5,12 @@ import Pusher from "pusher-js";
 import AxiosInstance from "@/lib/axiosInstance";
 // import { useQuery } from "@tanstack/react-query";
 import { NotificationTypes } from "@/types/NotificationType";
+import { useSelector } from "react-redux";
+import { RootState } from "@/redux/store";
 
 export const Notifications = () => {
+  const { username } = useSelector((state: RootState) => state.auth);
+
   const [notification, setNotification] = useState<NotificationTypes[]>([]);
   async function fetchNotification() {
     try {
@@ -15,8 +19,7 @@ export const Notifications = () => {
           Authorization: `Bearer ${Cookies.get("AUTH_TOKEN")}`,
         },
       });
-
-      setNotification(res.data.notifications);
+      setNotification(res.data.data);
     } catch (err) {
       throw err;
     }
@@ -41,14 +44,15 @@ export const Notifications = () => {
   useEffect(() => {
     const pusher = new Pusher("fac20a8dfab0cadc489c", {
       cluster: "ap2",
-      forceTLS: true,
-      wsPort: 443,
-      disableStats: true,
+      forceTLS: false,
+      // wsPort:80
     });
+    Pusher.logToConsole = true;
 
-    const channel = pusher.subscribe("notification");
-    channel.bind("notification", (data: any) => {
-        setNotification((prev: any) => [...prev, data.data]);
+    const channel = pusher.subscribe(`notification.${username}`);
+    channel.bind("notification.follow", (data: any) => {
+      console.log(data);
+      setNotification((prev: any) => [...prev, data.data]);
     });
 
     return () => {
@@ -62,7 +66,7 @@ export const Notifications = () => {
     <>
       <h1>Notification</h1>
 
-      {notification.map((data: any) => (
+      {notification?.map((data: any) => (
         <div key={data.id}>
           <p>{data.id}</p>
           <h1>{data.message}</h1>
