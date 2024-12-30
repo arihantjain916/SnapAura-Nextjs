ceb6d6aac77e52acb75f3a87aab2111dfe594e7e
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-30 09:53:38 +0530

    feat: make notification types

--- types/NotificationType.ts
+++ types/NotificationType.ts
@@ -1,6 +1,22 @@
+import { PostType } from "./PostType";
+import { UserType } from "./UserType";
+
+export interface FollowRequestResponse {
+  data: NotificationTypes;
+  user: UserType;
+  post: null | PostType;
+}
+
 export interface NotificationTypes {
-    id: string;
-    message: string;
-    senderId: string;
-    createdAt: Date;
+  id: string;
+  user_id: string;
+  message: string;
+  type: string;
+  link: string;
+  is_read: number;
+  read_at: null | string;
+  action_type: string;
+  created_at: string;
+  updated_at: string;
+  user: UserType;
 }
