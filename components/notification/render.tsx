cf222c7f38ebe82b832b708c969e9f5f25cb7ae9
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-12-31 12:51:51 +0530

    feat: render notification efficiently

--- components/notification/render.tsx
+++ components/notification/render.tsx
@@ -2,6 +2,8 @@ import { NotificationTypes } from "@/types/NotificationType";
 import { AlertCircle, Info, CheckCircle } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
+import Cookies from "js-cookie";
+import axios from "axios";
 
 export const NotificationRender = ({
   notification,
@@ -29,7 +31,22 @@ export const NotificationRender = ({
         return "border-success";
     }
   };
-  
+
+  async function handleFollow(link: string, id: string) {
+    try {
+      const res = await axios.get(`${link}?id=${id}`, {
+        headers: {
+          Authorization: `Bearer ${Cookies.get("AUTH_TOKEN")}`,
+        },
+      });
+
+      if (res.data.status) {
+        alert(res.data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className="space-y-2 mt-2 p-2">
       {notification.map((data) => (
@@ -38,20 +55,20 @@ export const NotificationRender = ({
           className={`${getToastClass(data.type)} w-full flex flex-col md:flex-row items-center justify-between gap-2 border-2 p-2`}
         >
           <div className="flex items-center gap-2">
-          {getIcon(data.type)}
+            {getIcon(data.type)}
             <Avatar>
               <AvatarImage src={data.meta.user.profile} />
               <AvatarFallback></AvatarFallback>
             </Avatar>
             <h1>{data.message}</h1>
           </div>
-          {data.action_type === "follow" && data.link && (
+          {data.action_type === "follow" && data.meta.link && (
             <Button
               onClick={() => {
-                console.log(data.link);
+                handleFollow(data.meta.link!, data.id);
               }}
             >
-              Follow
+              {data.meta.button_text}
             </Button>
           )}
         </div>
