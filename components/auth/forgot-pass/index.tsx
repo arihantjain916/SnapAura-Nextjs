685e5eb2d9da5c4d1892b56f61ac3f4d235b8116
arihant-getgrahak <arihant.jain@getgrahak.in>
2025-01-01 16:53:57 +0530

    feat: make password reset component working

--- components/auth/forgot-pass/index.tsx
+++ components/auth/forgot-pass/index.tsx
@@ -5,6 +5,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   Form,
   FormControl,
@@ -20,6 +22,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
+import AxiosInstance from "@/lib/axiosInstance";
 
 export const ForgotPassword = () => {
   const ForgotPass = z.object({
@@ -37,103 +40,96 @@ export const ForgotPassword = () => {
   });
 
   async function onFormSubmit(data: ForgotPassType) {
-    console.log(data);
+    try {
+      const res = await AxiosInstance.post(
+        "/user/reset/password?_method=PUT",
+        data
+      );
+      if (res.data.status === "success") {
+        toast.success(res.data.message, {
+          position: "bottom-right",
+        });
+      }
+    } catch (error: any) {
+      if (error.response.status === 422) {
+        toast.warn(error.response.data.message, {
+          position: "bottom-right",
+        });
+      }
+    }
   }
 
   return (
-    <Dialog>
-      <DialogTrigger>Forgot Password</DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <div className="w-full rounded-lg p-6 shadow sm:max-w-md sm:p-8 md:mt-0">
-            <DialogTitle className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Forgot Password
-            </DialogTitle>
+    <>
+      <ToastContainer />
+      <Dialog>
+        <DialogTrigger>Forgot Password</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <div className="w-full rounded-lg p-6 shadow sm:max-w-md sm:p-8 md:mt-0">
+              <DialogTitle className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Forgot Password
+              </DialogTitle>
 
-            <p className="font-light text-gray-500 dark:text-gray-400">
-              Don't fret! Just type in your email and password!
-            </p>
-            <Form {...forgotPasswordForm}>
-              <form
-                className="mt-4 space-y-4 md:space-y-5 lg:mt-5"
-              >
-                <div>
-                  <FormField
-                    control={forgotPasswordForm.control}
-                    name="email"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Email*</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="email"
-                            placeholder="m@exp.com"
-                            {...field}
-                            required
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div>
-                  <FormField
-                    control={forgotPasswordForm.control}
-                    name="password"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Password*</FormLabel>
-                        <FormControl>
-                          <Input
-                            type="password"
-                            placeholder=""
-                            {...field}
-                            required
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div className="flex items-start">
-                  <div className="flex h-5 items-center">
-                    <input
-                      id="terms"
-                      aria-describedby="terms"
-                      type="checkbox"
-                      className="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
-                      required={true}
+              <p className="font-light text-gray-500 dark:text-gray-400">
+                Don't fret! Just type in your email and password!
+              </p>
+              <Form {...forgotPasswordForm}>
+                <form className="mt-4 space-y-4 md:space-y-5 lg:mt-5">
+                  <div>
+                    <FormField
+                      control={forgotPasswordForm.control}
+                      name="email"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Email*</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="email"
+                              placeholder="m@exp.com"
+                              {...field}
+                              required
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
                     />
                   </div>
-                  <div className="ml-3 text-sm">
-                    <Label
-                      htmlFor="terms"
-                      className="font-light text-gray-500 dark:text-gray-300"
-                    >
-                      I accept the{" "}
-                      <Link
-                        className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
-                        href="#"
-                      >
-                        Terms and Conditions
-                      </Link>
-                    </Label>
+                  <div>
+                    <FormField
+                      control={forgotPasswordForm.control}
+                      name="password"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Password*</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="password"
+                              placeholder=""
+                              {...field}
+                              required
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
                   </div>
-                </div>
-                <Button
-                  onClick={forgotPasswordForm.handleSubmit(onFormSubmit)}
-                  type="button"
-                  className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-                >
-                  Reset password
-                </Button>
-              </form>
-            </Form>
-          </div>
-        </DialogHeader>
-      </DialogContent>
-    </Dialog>
+
+                  <Button
+                    onClick={forgotPasswordForm.handleSubmit(onFormSubmit)}
+                    type="button"
+                    className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                  >
+                    Reset password
+                  </Button>
+                </form>
+              </Form>
+            </div>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
