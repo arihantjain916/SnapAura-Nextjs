2b94772977f95acd666c78ce511f276d100b6dec
arihant-getgrahak <arihant.jain@getgrahak.in>
2025-01-01 16:40:41 +0530

    feat: add react-hook-form to handle form values

--- components/auth/forgot-pass/index.tsx
+++ components/auth/forgot-pass/index.tsx
@@ -1,13 +1,45 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
 
 export const ForgotPassword = () => {
+  const ForgotPass = z.object({
+    email: z.string().email("This is not a valid email."),
+    password: z.string().min(3),
+  });
+
+  type ForgotPassType = z.infer<typeof ForgotPass>;
+  const forgotPasswordForm = useForm<ForgotPassType>({
+    resolver: zodResolver(ForgotPass),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onFormSubmit(data: ForgotPassType) {
+    console.log(data);
+  }
+
   return (
     <Dialog>
       <DialogTrigger>Forgot Password</DialogTrigger>
@@ -19,58 +51,86 @@ export const ForgotPassword = () => {
             </DialogTitle>
 
             <p className="font-light text-gray-500 dark:text-gray-400">
-              Don't fret! Just type in your email and we will send you a code to
-              reset your password!
+              Don't fret! Just type in your email and password!
             </p>
-            <form className="mt-4 space-y-4 md:space-y-5 lg:mt-5" action="#">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Your email
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="focus:ring-primary-600 focus:border-primary-600 block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                  placeholder="name@company.com"
-                  //   required=""
-                />
-              </div>
-              <div className="flex items-start">
-                <div className="flex h-5 items-center">
-                  <input
-                    id="terms"
-                    aria-describedby="terms"
-                    type="checkbox"
-                    className="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
-                    // required=""
+            <Form {...forgotPasswordForm}>
+              <form
+                className="mt-4 space-y-4 md:space-y-5 lg:mt-5"
+              >
+                <div>
+                  <FormField
+                    control={forgotPasswordForm.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email*</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="m@exp.com"
+                            {...field}
+                            required
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
                   />
                 </div>
-                <div className="ml-3 text-sm">
-                  <label
-                    htmlFor="terms"
-                    className="font-light text-gray-500 dark:text-gray-300"
-                  >
-                    I accept the{" "}
-                    <a
-                      className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
-                      href="#"
-                    >
-                      Terms and Conditions
-                    </a>
-                  </label>
+                <div>
+                  <FormField
+                    control={forgotPasswordForm.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password*</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder=""
+                            {...field}
+                            required
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
                 </div>
-              </div>
-              <button
-                type="submit"
-                className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
-              >
-                Reset password
-              </button>
-            </form>
+                <div className="flex items-start">
+                  <div className="flex h-5 items-center">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 h-4 w-4 rounded border border-gray-300 bg-gray-50 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800"
+                      required={true}
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <Label
+                      htmlFor="terms"
+                      className="font-light text-gray-500 dark:text-gray-300"
+                    >
+                      I accept the{" "}
+                      <Link
+                        className="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </Link>
+                    </Label>
+                  </div>
+                </div>
+                <Button
+                  onClick={forgotPasswordForm.handleSubmit(onFormSubmit)}
+                  type="button"
+                  className="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4"
+                >
+                  Reset password
+                </Button>
+              </form>
+            </Form>
           </div>
         </DialogHeader>
       </DialogContent>
